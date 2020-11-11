78c938d2db8306fb952c20e5730e042eeeb9cb00
Diego Fernando Caviedes Camacho <diego_1996.c.c@hotmail.com>
2020-11-11 14:06:14 -0500

    colocando return de la ruta en la plantilla blade

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 
 Route::get('/test', function () {
     return view('test',[
-        'title'=> 'Curso de Larevel viene'
+        'title'=> 'Curso de Larevel vieneeeee'
     ]);
 });
